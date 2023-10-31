442743767bd4f169fbfc942e18b379d9c2f331a0
Nisha Bukke <sononenisha827@gmail.com>
2023-10-31 11:13:21 +0530

    Ruturang Paints

---
+++ javascript.js
@@ -0,0 +1,133 @@
+
+
+// loader js
+
+document.onreadystatechange = function () {
+if (document.readyState !== "complete") {
+    document.querySelector(
+        "body").style.visibility = "hidden";
+    document.querySelector(
+        "#preloader").style.visibility = "visible";
+} else {
+    document.querySelector(
+        "#preloader").style.display = "none";
+    document.querySelector(
+        "body").style.visibility = "visible";
+}
+};
+
+
+
+
+// fade effects
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+// tabs togggling
+
+  function activeTab(evt, id) {
+           
+    // Get all elements with class="tablinks" and remove the class "active"
+     let tabactive = document.getElementsByClassName("TabButtonSelected");
+     tabactive[0].className = tabactive[0].className.replace(" TabButtonSelected", "");
+
+     document.getElementById(id).style.display = "block";
+     evt.currentTarget.className += " TabButtonSelected";
+
+     displaySection(evt,id)
+ }
+ 
+function displaySection(evt, id) {
+
+     let tabactive = document.getElementsByClassName("tab-section");
+     tabactive[0].className = tabactive[0].className.replace(" d-chart-show", "d-chart-n");
+     // add below line of codes
+     [...document.querySelectorAll('div.tab-section')].forEach(item => item.style.display='none')
+     document.getElementById("Section" + id).style.display = "block";
+     evt.currentTarget.className += " d-chart-show";
+
+}
+
+
+// product detail select option div toggle
+
+$(document).on('change', '.div-toggle', function() {
+  var target = $(this).data('target');
+  var show = $("option:selected", this).data('show');
+  $(target).children().addClass('hide');
+  $(show).removeClass('hide');
+});
+$(document).ready(function(){
+	$('.div-toggle').trigger('change');
+});
+
+
+// mobile menu
+
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+  });
+  });
+  
+  });
+
+ // Close out sub menu
+ $('.sub__close').click(function(e) {
+  e.preventDefault();
+  
+  $(this).parent().parent().removeClass('is-active');
+});
+
+// Trigger sub menu
+$('.menu ul .nav__submenu').click(function(e) {
+  e.preventDefault();
+  
+  $(this).siblings().addClass('is-active');
+});
+
+// window size banner
+  
+function jqUpdateSize(){
+  // Get the dimensions of the viewport
+  // var width = $(window).width();
+  var height = $(window).height();
+
+  // $('#jqWidth').html(width);
+  $('#jqHeight').html(height);
+
+  $('.slide-show-container').css({ 
+  height:  $(window).height(),
+  overflow:" hidden"});
+
+}
+$(document).ready(jqUpdateSize);    // When the page first loads
+$(window).resize(jqUpdateSize);     // When the browser changes size
