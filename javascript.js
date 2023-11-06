db662506cbc789af8bddbb41668f429ebcba1e3c
Nisha Bukke <sononenisha827@gmail.com>
2023-11-06 13:53:44 +0530

    added vectors and client changes

--- javascript.js
+++ javascript.js
@@ -17,6 +17,63 @@ if (document.readyState !== "complete") {
 };
 
 
+// vertical slider
+
+// class Slider {
+//   constructor(options) {
+//     this.sections = document.querySelectorAll(options.section);
+//     this.navigation = document.querySelector(options.dots);
+
+//     this.navigation.addEventListener('click', this.scrollToSection.bind(this));
+//     window.addEventListener('scroll', this.setDotStatus.bind(this));
+//   }
+
+//   removeDotStyles() {
+//     const dots = this.navigation;
+//     const is_active = dots.querySelector('.is-active');
+
+//     if (is_active != null) {
+//       is_active.classList.remove('is-active');
+//     }
+//   }
+
+//   setDotStatus() {
+//     const scroll_position = window.scrollY;
+//     const dots = Array.from(this.navigation.children);
+
+//     this.sections.forEach((section, index) => {
+//       const half_window = window.innerHeight / 2;
+//       const section_top = section.offsetTop;
+
+//       if (scroll_position > section_top - half_window && scroll_position < section_top + half_window) {
+//         this.removeDotStyles();
+//         dots[index].classList.add('is-active');
+//       }
+//     })
+//   }
+
+//   scrollToSection(e) {
+//     const dots = Array.from(this.navigation.children);
+//     const window_height = window.innerHeight;
+
+//     dots.forEach((dot, index) => {
+//       if (dot == e.target) {
+
+//         window.scrollTo({
+//           top: window_height * index,
+//           behavior: 'smooth',
+//         });
+//       }
+//     });
+//   }
+// }
+
+// new Slider({
+//   section: '.section',
+//   dots: '#js-dots',
+// });
+
+
 
 
 // fade effects
@@ -72,7 +129,6 @@ $(document).on('change', '.div-toggle', function() {
   var show = $("option:selected", this).data('show');
   $(target).children().addClass('hide');
   $(show).removeClass('hide');
-  
 });
 $(document).ready(function(){
 	$('.div-toggle').trigger('change');
